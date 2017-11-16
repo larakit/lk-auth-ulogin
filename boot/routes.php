58141c9f9ad529c655e9c3ffce66b2143354f74d
berdnikov <aberdnikov@gmail.com>
2017-11-16 14:16:06 +0500

    fixed

--- boot/routes.php
+++ boot/routes.php
@@ -15,13 +15,14 @@ Route::post('/logout', function () {
 })->name('logout')->middleware('web');
 
 Route::get('/login', function () {
-    return '
+    return \Larakit\Page\LkPage::instance()->setBodyContent('
 <script src="//ulogin.ru/js/ulogin.js"></script>
 <div style="width: 100%; margin:auto;height: 100vh; padding-top:300px; text-align: center">
 <h1>' . config('larakit.lk-auth-ulogin.title') . '</h1>
+<br>
 <div id="uLogin"
-data-ulogin="display=' . config('larakit.lk-auth-ulogin.display') . ';theme=classic;fields=' . implode(',', config('larakit.lk-auth-ulogin.fields')) . ';providers=' . implode(',', config('larakit.lk-auth-ulogin.providers')) . ';hidden=' . config('larakit.lk-auth-ulogin.hidden') . ';redirect_uri=' . urlencode(route('login') . '?_token=' . csrf_token()) . ';mobilebuttons=0;"></div>                        
-                        </div>';
+data-ulogin="display=' . config('larakit.lk-auth-ulogin.display') . ';theme=classic;fields=' . implode(',', config('larakit.lk-auth-ulogin.fields')) . ';providers=' . implode(',', config('larakit.lk-auth-ulogin.providers')) . ';hidden=' . config('larakit.lk-auth-ulogin.hidden') . ';redirect_uri=' . urlencode(route('login') . '?_token=' . csrf_token()) . ';mobilebuttons=0;"></div>
+                        </div>');
 })->name('login')->middleware('web')->middleware('guest');
 
 Route::post('/login', function () {
